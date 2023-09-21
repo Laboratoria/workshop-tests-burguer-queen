e01eabf45f61f9523392837fbd8d07a437844e35
David <davidgranados73@gmail.com>
2023-09-21 09:10:57 -0500

    logear errores del servicio y no usar archivo de mocks

--- src/pages/login/login.test.tsx
+++ src/pages/login/login.test.tsx
@@ -1,40 +1,6 @@
 import "@testing-library/jest-dom";
 
 describe("Login", () => {
-  it("debe mostrar un mensaje de error si los credenciales son incorrectos", async () => {
-    /* 1. Arrange - Preparar
-
-      Prepare los requisitos previos para nuestra prueba.
-
-      ejemplo:
-        - mockear una función/servicio
-        - renderizar un componente correctamente
-    */
-    // mockear el servicio
-    // renderizar el componente
-    // obtener los elementos del DOM con los que vamos a interactuar
-    /* 2. Act - Actuar / Interactuar
-
-      Interactúe con el componente que está probando.
-
-      ejemplo:
-        - hacer clic en un botón
-        - escribir en un campo de texto
-    */
-    // llenar los campos del formulario
-    // hacer clic en el botón de submit
-    /* 3. Assert - Asegurarse / Afirmar
-
-      Asegurarse que el resultado de la interacción es el esperado.
-
-      ejemplo:
-        - verificar que se renderizó un elemento
-        - verificar que se mostró un mensaje de error
-    */
-    // verificar que se muestra el mensaje de error
-    // screen.debug();
-  });
-
   it("debe redirigir a la página de inicio si los credenciales son correctos", async () => {
     /* 1. Arrange - Preparar
 
@@ -67,6 +33,40 @@ describe("Login", () => {
         - verificar que se mostró un mensaje de error
     */
     // verificar que se renderizó el componente Home
+    // screen.debug();
+  });
+
+  it("debe mostrar un mensaje de error si los credenciales son incorrectos", async () => {
+    /* 1. Arrange - Preparar
+
+      Prepare los requisitos previos para nuestra prueba.
+
+      ejemplo:
+        - mockear una función/servicio
+        - renderizar un componente correctamente
+    */
+    // mockear el servicio
+    // renderizar el componente
+    // obtener los elementos del DOM con los que vamos a interactuar
+    /* 2. Act - Actuar / Interactuar
+
+      Interactúe con el componente que está probando.
+
+      ejemplo:
+        - hacer clic en un botón
+        - escribir en un campo de texto
+    */
+    // llenar los campos del formulario
+    // hacer clic en el botón de submit
+    /* 3. Assert - Asegurarse / Afirmar
+
+      Asegurarse que el resultado de la interacción es el esperado.
+
+      ejemplo:
+        - verificar que se renderizó un elemento
+        - verificar que se mostró un mensaje de error
+    */
+    // verificar que se muestra el mensaje de error
     // screen.debug();
   });
 });

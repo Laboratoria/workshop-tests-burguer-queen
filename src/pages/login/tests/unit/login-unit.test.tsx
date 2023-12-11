216c47e1f45f25e6e473dded5ccdb91f2e658f10
David <davidgranados73@gmail.com>
2023-12-11 10:00:18 -0500

    login unit test

---
+++ src/pages/login/tests/unit/login-unit.test.tsx
@@ -0,0 +1,45 @@
+describe("Login", () => {
+  it.only("debe llamar la función navigate con el path '/' si los credenciales son correctos", async () => {
+    // --------------------------------------------------------
+    /* 1. Arrange - Preparar
+
+      Prepare los requisitos previos para nuestra prueba.
+
+      ejemplo:
+        - mockear una función/servicio
+        - renderizar un componente correctamente
+    */
+    // --------------------------------------------------------
+
+    // 1.1 renderizar el un router con el componente Login
+    // 1.2 obtener los elementos del DOM con los que vamos a interactuar
+
+    // --------------------------------------------------------
+    /* 2. Act - Actuar / Interactuar
+
+      Interactúe con el componente que está probando.
+
+      ejemplo:
+        - hacer clic en un botón
+        - escribir en un campo de texto
+    */
+    // --------------------------------------------------------
+
+    // 2.1 llenar los campos del formulario
+
+    // 2.2 hacer clic en el botón de submit
+
+    // --------------------------------------------------------
+    /* 3. Assert - Asegurarse / Afirmar
+
+      Asegurarse que el resultado de la interacción es el esperado.
+
+      ejemplo:
+        - verificar que se renderizó un elemento
+        - verificar que se mostró un mensaje de error
+    */
+    // --------------------------------------------------------
+
+    // verificar que se llamo la función navigate con el path "/"
+  });
+});
